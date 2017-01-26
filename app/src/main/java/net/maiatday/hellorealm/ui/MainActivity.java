dac27e05298b014933df41359db5f3725777bf38
maiatoday <maiatoday@gmail.com>
2017-01-26 12:07:01 +0200

    Add second activity

--- app/src/main/java/net/maiatday/hellorealm/ui/MainActivity.java
+++ app/src/main/java/net/maiatday/hellorealm/ui/MainActivity.java
@@ -1,169 +1,32 @@
 package net.maiatday.hellorealm.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import net.maiatday.hellorealm.R;
-import net.maiatday.hellorealm.model.Mood;
-import net.maiatday.hellorealm.service.WorkerIntentService;
-
-import java.util.Date;
-import java.util.UUID;
-
-import io.realm.Realm;
-import io.realm.RealmChangeListener;
-import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    Realm realm;
-    Mood firstMood;
-    private TextView textView;
-    private EditText editTextMain;
-    private EditText editTextWorker;
-    private ImageView currentMood;
-    private SeekBar energyBar;
+
+    private static final int REQUEST_NEW_MOOD = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.text_first_mood);
-        editTextMain = (EditText) findViewById(R.id.text_main);
-        editTextWorker = (EditText) findViewById(R.id.text_worker);
-        energyBar = (SeekBar) findViewById(R.id.energyBar);
-        energyBar.setOnSeekBarChangeListener(seekBarChangeListener);
-        currentMood = (ImageView) findViewById(R.id.imageCurrentMood);
-        realm = Realm.getDefaultInstance();
-        realm.addChangeListener(realmChangeListener);
-        RealmResults<Mood> moods = realm.where(Mood.class).findAll();
-        if (moods.isEmpty()) {
-            realm.executeTransaction(new Realm.Transaction() {
-                @Override
-                public void execute(Realm realm) {
-                    Mood m = realm.createObject(Mood.class);
-                    m.setId(UUID.randomUUID().toString());
-                    m.setEnergyLevel(-2);
-                    m.setMood(Mood.MEH);
-                    m.setNote("testing testing");
-                    m.setTimestamp(new Date());
-                }
-            });
-        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        for (Mood m : moods) {
-            Log.d(TAG, "mood: " + m.getMood() + " " + m.getNote() + " " + m.getId());
-        }
-        firstMood = realm.where(Mood.class).findFirst();
-        updateUI();
-    }
-
-    private void updateUI() {
-        if (textView != null &&
-                energyBar != null &&
-                currentMood != null &&
-                firstMood != null) {
-            //energyBar.setProgress(firstMood.getEnergyLevel(), true);
-            textView.setText(firstMood.toString());
-            switch (firstMood.getMood()) {
-                case Mood.SUPER_SAD:
-                    currentMood.setImageResource(R.drawable.ic_mood_super_sad);
-                    break;
-                case Mood.SAD:
-                    currentMood.setImageResource(R.drawable.ic_mood_sad);
-                    break;
-                case Mood.MEH:
-                    currentMood.setImageResource(R.drawable.ic_mood_meh);
-                    break;
-                case Mood.HAPPY:
-                    currentMood.setImageResource(R.drawable.ic_mood_happy);
-                    break;
-                case Mood.SUPER_HAPPY:
-                    currentMood.setImageResource(R.drawable.ic_mood_super_happy);
-                    break;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(OneMoodActivity.newIntent(MainActivity.this, ""), REQUEST_NEW_MOOD);
             }
-        }
+        });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        realm.removeAllChangeListeners();
-        realm.close();
-    }
-
-    public void onClick(View view) {
-        if (view.getId() == R.id.button_update_main) {
-            realm.beginTransaction();
-            Mood m = realm.where(Mood.class).findFirst();
-            m.setNote(editTextMain.getText().toString());
-            m.setTimestamp(new Date());
-            realm.commitTransaction();
-        } else {
-            WorkerIntentService.startUpdateFirstNote(this, editTextWorker.getText().toString());
-        }
-    }
-
-    public void onMoodClick(View view) {
-        int id = view.getId();
-        @Mood.PossibleMood int mood = Mood.MEH;
-        switch (id) {
-            case R.id.buttonSuperSad:
-                mood = Mood.SUPER_SAD;
-                break;
-            case R.id.buttonSad:
-                mood = Mood.SAD;
-                break;
-            case R.id.buttonMeh:
-                mood = Mood.MEH;
-                break;
-            case R.id.buttonHappy:
-                mood = Mood.HAPPY;
-                break;
-            case R.id.buttonSuperHappy:
-                mood = Mood.SUPER_HAPPY;
-                break;
-        }
-        realm.beginTransaction();
-        Mood m = realm.where(Mood.class).findFirst();
-        m.setMood(mood);
-        m.setTimestamp(new Date());
-        realm.commitTransaction();
-    }
-
-
-    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-            realm.beginTransaction();
-            Mood m = realm.where(Mood.class).findFirst();
-            m.setEnergyLevel(progressValue);
-            m.setTimestamp(new Date());
-            realm.commitTransaction();
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-           // do nothing
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-           // do nothing
-        }
-    };
-
-
-    private RealmChangeListener<Realm> realmChangeListener = new RealmChangeListener<Realm>() {
-        @Override
-        public void onChange(Realm element) {
-            updateUI();
-        }
-    };
 }
---
+++ app/src/main/java/net/maiatday/hellorealm/ui/OneMoodActivity.java
@@ -0,0 +1,182 @@
+package net.maiatday.hellorealm.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import net.maiatday.hellorealm.R;
+import net.maiatday.hellorealm.model.Mood;
+import net.maiatday.hellorealm.service.WorkerIntentService;
+
+import java.util.Date;
+import java.util.UUID;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+
+public class OneMoodActivity extends AppCompatActivity {
+    private static final String TAG = "OneMoodActivity";
+    private static final String KEY_MOOD_UUID = "mood_uuid";
+    Realm realm;
+    Mood firstMood;
+    private TextView textView;
+    private EditText editTextMain;
+    private EditText editTextWorker;
+    private ImageView currentMood;
+    private SeekBar energyBar;
+    private String uuid;
+
+    public static Intent newIntent(Context context, String uuid) {
+        Intent i = new Intent(context, OneMoodActivity.class);
+        i.putExtra(KEY_MOOD_UUID, uuid);
+        return i;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uuid = getIntent().getStringExtra(KEY_MOOD_UUID);
+        setContentView(R.layout.activity_one_mood);
+        textView = (TextView) findViewById(R.id.text_first_mood);
+        editTextMain = (EditText) findViewById(R.id.text_main);
+        editTextWorker = (EditText) findViewById(R.id.text_worker);
+        energyBar = (SeekBar) findViewById(R.id.energyBar);
+        energyBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        currentMood = (ImageView) findViewById(R.id.imageCurrentMood);
+        realm = Realm.getDefaultInstance();
+        realm.addChangeListener(realmChangeListener);
+        RealmResults<Mood> moods = realm.where(Mood.class).findAll();
+        if (TextUtils.isEmpty(uuid)) {
+            realm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    Mood m = realm.createObject(Mood.class);
+                    uuid = UUID.randomUUID().toString();
+                    m.setId(uuid);
+                    m.setEnergyLevel(-2);
+                    m.setMood(Mood.MEH);
+                    m.setNote("testing testing");
+                    m.setTimestamp(new Date());
+                }
+            });
+        }
+
+        for (Mood m : moods) {
+            Log.d(TAG, "mood: " + m.getMood() + " " + m.getNote() + " " + m.getId());
+        }
+        firstMood = realm.where(Mood.class).equalTo("id", uuid).findFirst();
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (textView != null &&
+                energyBar != null &&
+                currentMood != null &&
+                firstMood != null) {
+            //energyBar.setProgress(firstMood.getEnergyLevel(), true);
+            textView.setText(firstMood.toString());
+            switch (firstMood.getMood()) {
+                case Mood.SUPER_SAD:
+                    currentMood.setImageResource(R.drawable.ic_mood_super_sad);
+                    break;
+                case Mood.SAD:
+                    currentMood.setImageResource(R.drawable.ic_mood_sad);
+                    break;
+                case Mood.MEH:
+                    currentMood.setImageResource(R.drawable.ic_mood_meh);
+                    break;
+                case Mood.HAPPY:
+                    currentMood.setImageResource(R.drawable.ic_mood_happy);
+                    break;
+                case Mood.SUPER_HAPPY:
+                    currentMood.setImageResource(R.drawable.ic_mood_super_happy);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.removeAllChangeListeners();
+        realm.close();
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_update_main) {
+            realm.beginTransaction();
+            Mood m = realm.where(Mood.class).findFirst();
+            m.setNote(editTextMain.getText().toString());
+            m.setTimestamp(new Date());
+            realm.commitTransaction();
+        } else {
+            WorkerIntentService.startUpdateFirstNote(this, editTextWorker.getText().toString());
+        }
+    }
+
+    public void onMoodClick(View view) {
+        int id = view.getId();
+        @Mood.PossibleMood int mood = Mood.MEH;
+        switch (id) {
+            case R.id.buttonSuperSad:
+                mood = Mood.SUPER_SAD;
+                break;
+            case R.id.buttonSad:
+                mood = Mood.SAD;
+                break;
+            case R.id.buttonMeh:
+                mood = Mood.MEH;
+                break;
+            case R.id.buttonHappy:
+                mood = Mood.HAPPY;
+                break;
+            case R.id.buttonSuperHappy:
+                mood = Mood.SUPER_HAPPY;
+                break;
+        }
+        realm.beginTransaction();
+        Mood m = realm.where(Mood.class).findFirst();
+        m.setMood(mood);
+        m.setTimestamp(new Date());
+        realm.commitTransaction();
+    }
+
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+            realm.beginTransaction();
+            Mood m = realm.where(Mood.class).findFirst();
+            m.setEnergyLevel(progressValue);
+            m.setTimestamp(new Date());
+            realm.commitTransaction();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // do nothing
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // do nothing
+        }
+    };
+
+
+    private RealmChangeListener<Realm> realmChangeListener = new RealmChangeListener<Realm>() {
+        @Override
+        public void onChange(Realm element) {
+            updateUI();
+        }
+    };
+}
