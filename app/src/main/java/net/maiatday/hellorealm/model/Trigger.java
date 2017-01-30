efa01dca7790a3ec241dbbe9b9fed26cce650df3
maiatoday <maiatoday@gmail.com>
2017-01-30 17:25:40 +0200

    Updated copyright notice.

--- app/src/main/java/net/maiatday/hellorealm/model/Trigger.java
+++ app/src/main/java/net/maiatday/hellorealm/model/Trigger.java
@@ -1,21 +1,36 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Maia Grotepass
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.maiatday.hellorealm.model;
 
-import android.support.annotation.DrawableRes;
-import android.support.annotation.IntDef;
-
-import net.maiatday.hellorealm.R;
-
-import java.lang.annotation.Retention;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
-
-import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 /**
  * Class to store trigger
